163f54442eacdd89048f8512bf2615a867e221b3
ulyana <ulyanaworkpage@gmail.com>
2025-06-08 19:24:11 +0300

    Tortoise config

--- app/main.py
+++ app/main.py
@@ -3,13 +3,23 @@ from tortoise.contrib.fastapi import register_tortoise
 from app.api import api_router
 
 app = FastAPI()
-
 app.include_router(api_router)
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": "postgres://postgres:pass1234@localhost:5432/juice",
+    },
+    "apps": {
+        "models": {
+            "models": ["app.core.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
 
 register_tortoise(
     app,
-    db_url="postgres://postgres:pass1234@localhost:5432/juice",
-    modules={"models": ["app.core.models"]},
-    generate_schemas=False,
+    config=TORTOISE_ORM,
+    generate_schemas=False,  # миграции управляются Aerich
     add_exception_handlers=True,
 )
